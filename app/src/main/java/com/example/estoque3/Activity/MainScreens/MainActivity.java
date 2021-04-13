aee57b4ad8365e414dd3e14a94bc43bd81ff53e9
gabriel <gabriel@DESKTOP-CUJ2S93>
2021-04-12 22:46:23 -0300

    Main

---
+++ app/src/main/java/com/example/estoque3/Activity/MainScreens/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.estoque3.Activity.MainScreens;
+
+import android.content.Intent;
+import android.os.Bundle;
+import com.example.estoque3.Activity.AddProductActivity;
+import com.example.estoque3.Activity.AddSaleActivity;
+import com.example.estoque3.R;
+import com.example.estoque3.TestActivity;
+import com.example.estoque3.entity.Product;
+import com.example.estoque3.ui.main.PageViewModel;
+import com.example.estoque3.util.AdapterProduct;
+import com.google.android.material.tabs.TabLayout;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.estoque3.ui.main.SectionsPagerAdapter;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        viewPager = findViewById(R.id.view_pager);
+        viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabs = findViewById(R.id.tabs);
+        tabs.setupWithViewPager(viewPager);
+    }
+
+
+    public void Create(View view) {
+        if (viewPager.getCurrentItem()==0){
+            startActivity(new Intent(getApplicationContext(), TestActivity.class));
+        }else if (viewPager.getCurrentItem()==1){
+            startActivity(new Intent(getApplicationContext(), AddProductActivity.class));
+        }else if (viewPager.getCurrentItem()==2){
+            startActivity(new Intent(getApplicationContext(), AddSaleActivity.class));
+        }
+    }
+}
+
+
